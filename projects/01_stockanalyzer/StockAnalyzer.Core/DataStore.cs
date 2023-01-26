57585abedd5964ba5ad9d4ca44de26965e9cc75d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-01-26 08:44:22 +0100

    Cleaned up the solution

--- projects/01_stockanalyzer/StockAnalyzer.Core/DataStore.cs
+++ projects/01_stockanalyzer/StockAnalyzer.Core/DataStore.cs
@@ -6,62 +6,61 @@ using System.Linq;
 using System.Threading.Tasks;
 using StockAnalyzer.Core.Domain;
 
-namespace StockAnalyzer.Core
+namespace StockAnalyzer.Core;
+
+public class DataStore
 {
-    public class DataStore
+    private string basePath { get; }
+
+    public DataStore(string basePath = "")
     {
-        private string basePath { get; }
+        this.basePath = basePath;
+    }
 
-        public DataStore(string basePath = "")
+    public async Task<IList<StockPrice>> GetStockPrices(string stockIdentifier)
+    {
+        var prices = new List<StockPrice>();
+
+        using (var stream =
+               new StreamReader(File.OpenRead(Path.Combine(basePath, @"StockPrices_Small.csv"))))
         {
-            this.basePath = basePath;
-        }
+            await stream.ReadLineAsync(); // Skip the header how in the CSV
 
-        public async Task<IList<StockPrice>> GetStockPrices(string stockIdentifier)
-        {
-            var prices = new List<StockPrice>();
-
-            using (var stream =
-                new StreamReader(File.OpenRead(Path.Combine(basePath, @"StockPrices_Small.csv"))))
+            string line;
+            while ((line = await stream.ReadLineAsync()) != null)
             {
-                await stream.ReadLineAsync(); // Skip the header how in the CSV
+                #region Find & Parse Stock Price from CSV
+                var segments = line.Split(',');
 
-                string line;
-                while ((line = await stream.ReadLineAsync()) != null)
-                {
-                    #region Find & Parse Stock Price from CSV
-                    var segments = line.Split(',');
-
-                    for (var i = 0; i < segments.Length; i++) segments[i] = segments[i].Trim('\'', '"');
+                for (var i = 0; i < segments.Length; i++) segments[i] = segments[i].Trim('\'', '"');
                    
-                    if(segments[0].ToUpperInvariant() 
-                        != stockIdentifier.ToUpperInvariant())
-                    {
-                        continue;
-                    }
-                    #endregion
-
-                    var price = new StockPrice
-                    {
-                        Identifier = segments[0],
-                        TradeDate = DateTime.ParseExact(segments[1], "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
-                        Volume = Convert.ToInt32(segments[6], CultureInfo.InvariantCulture),
-                        Change = Convert.ToDecimal(segments[7], CultureInfo.InvariantCulture),
-                        ChangePercent = Convert.ToDecimal(segments[8], CultureInfo.InvariantCulture),
-                    };
-
-                    prices.Add(price);
+                if(segments[0].ToUpperInvariant() 
+                   != stockIdentifier.ToUpperInvariant())
+                {
+                    continue;
                 }
+                #endregion
+
+                var price = new StockPrice
+                {
+                    Identifier = segments[0],
+                    TradeDate = DateTime.ParseExact(segments[1], "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                    Volume = Convert.ToInt32(segments[6], CultureInfo.InvariantCulture),
+                    Change = Convert.ToDecimal(segments[7], CultureInfo.InvariantCulture),
+                    ChangePercent = Convert.ToDecimal(segments[8], CultureInfo.InvariantCulture),
+                };
+
+                prices.Add(price);
             }
-
-            if(!prices.Any())
-            {
-                throw new KeyNotFoundException($"Could not find any stocks for {stockIdentifier}");
-            }
-
-            await Task.Delay(5000);
-
-            return prices;
         }
+
+        if(!prices.Any())
+        {
+            throw new KeyNotFoundException($"Could not find any stocks for {stockIdentifier}");
+        }
+
+        await Task.Delay(5000);
+
+        return prices;
     }
 }
